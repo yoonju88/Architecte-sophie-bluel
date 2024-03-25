0d194fc4db6f954adeb905ec1d587f48f787814f
yoonju88 <yoonju.t@gmail.com>
2024-03-25 15:51:09 +0100

    add la section gallery

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,25 +1,47 @@
+//Base de URL
 const baseUrl = "http://localhost:5678/api/"
-//récup post
-const loginUser = await fetch (baseUrl+ "users/login",{
-    method : "POST", 
-    headers : {
-        "Content-Type" : "application/json"
+//Récup POST users/login
+const loginUser = await fetch(baseUrl + "users/login", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
     },
-    body : JSON.stringify ({  
+    body: JSON.stringify({
         "email": "sophie.bluel@test.tld",
         "password": "S0phie"
     })
 })
-const loginUserResponse = await loginUser.json ()
+const loginUserResponse = await loginUser.json()
+//Récup GET categories
+const categories = await fetch(baseUrl + "categories", { method: "GET"})
+const categoriesResponse = await categories.json()
+//Récup GET works
+const getGallery = await fetch(baseUrl + "works", {
+    method: "GET"
+    })
 
+const galleries = await getGallery.json()
 
-const categories = await fetch (baseUrl + "categories",{
-    method : "GET"
-})
-const categoriesReponse = await categories.json()
+function createGallery(galleries) {    
+    for (let i = 0; i < galleries.length; i++ ) {   
 
-const ReturnsWorks = await fetch (baseUrl + "works",{
-    method : "GET"
-})
-const ReturnsWorksReponse = await ReturnsWorks.json()
+    const infoGalleries = galleries[i]
+
+    const sectionGallery = document.querySelector(".gallery")
+    const figureElement = document.createElement("figure")
+    sectionGallery.appendChild(figureElement)
+
+    const imageElement = document.createElement("img")
+    imageElement.src = infoGalleries.imageUrl
+    imageElement.alt = infoGalleries.title
+    figureElement.appendChild(imageElement)
+
+    const figcaptionElement = document.createElement("figcaption")
+    figcaptionElement.innerText = infoGalleries.title
+    figureElement.appendChild(figcaptionElement)
+    }
+}
+createGallery(galleries);
+ // Récup element catégorie dans galleries array
+const categoryGalleries = galleries.map (galleries => galleries.category)
 
