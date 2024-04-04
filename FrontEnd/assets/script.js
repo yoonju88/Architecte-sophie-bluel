c2be13c00dbfab66ae901bc8ea6f401283f242dd
yoonju88 <yoonju.t@gmail.com>
2024-04-04 22:28:29 +0200

    add  fichier form.js

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,9 +1,12 @@
 import { addValidationLogin, addLogout } from "./login.js"
 import { addGenerateGallery, GenerateModalGallery } from "./gallery.js"
 import { displayModal } from "./edition.js"
+import { addFormData } from "./form.js"
+
 
 addValidationLogin()
 addLogout()
 addGenerateGallery()
 GenerateModalGallery()
 displayModal()
+addFormData()
