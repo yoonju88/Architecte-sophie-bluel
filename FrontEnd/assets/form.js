bf6e2b1d4cd47e42fb859dc47d5a22ead8c29bd0
yoonju88 <yoonju.t@gmail.com>
2024-04-08 10:46:12 +0200

    after session

--- FrontEnd/assets/form.js
+++ FrontEnd/assets/form.js
@@ -1,42 +1,61 @@
+/*
 const baseUrl = "http://localhost:5678/api/"
 
 const deleteWorks = await fetch(baseUrl + "works/{id}", {
     method: "DELETE",
     headers: {
-        "Accept": "*/*"
+        "Accept": "*'/*"
     }
 })
 const deleteWorksResponse = await deleteWorks.json()
 console.log("Success deleteWorks", deleteWorksResponse)
+*/
 
+async function sendImageToBackend(file, title, category){
 
-export async function addFormData() {
+    console.log(file);
+
+    const saveToken = localStorage.getItem("token")
+    if (!saveToken) { return; }
+
     const baseUrl = "http://localhost:5678/api/"
-    const formModal = document.getElementById("formModal")
-    if (!formModal) { console.error("Form not found"); return }
-
-    const formData = new FormData(formModal)
-    const inputFile = document.getElementById('file')
-    const fileImage = document.querySelector(".displayImage")
-    const titleValue = document.getElementById('title').value
-    const categoryValue = document.getElementById('category').value
-    formData.append('file', inputFile.files[0])
-    formData.append('title', titleValue)
-    formData.append('category', categoryValue)
+    
+    const formData = new FormData()
+    formData.append('image', file)
+    formData.append('title', title)
+    formData.append('category', category)
 
     try {
     const postWorks = await fetch(baseUrl + "works", {
         method: "POST",
         headers: {
-            "Accept": "application/json",
+            "Authorization": "Bearer " + saveToken
         },
         body: formData
     })
-    const postWorksResponse = await postWorks.json()
+    const postWorksResponse = await postWorks.text()
     console.log(postWorksResponse)
     }catch(error){
-        console.error('Error', error)
+        //console.error()
     }
+}
+
+export async function addFormData() {
+
+    const formModal = document.getElementById("formModal")
+    if (!formModal) { console.error("Form not found"); return }
+
+    const inputFile = document.getElementById('file')
+    const fileImage = document.querySelector(".displayImage")
+    const titleValue = document.getElementById('title')
+    const categoryValue = document.getElementById('category')
+
+    const valideImage = document.querySelector('.valideImage')
+
+    valideImage.addEventListener('click', e => {
+        e.preventDefault();
+        sendImageToBackend(inputFile.files[0], titleValue.value, categoryValue.value);
+    });
 
     //input에 이미지 경로를 값을 넣으면 이미지를 보여줌
     inputFile.addEventListener("change", function (e) {
@@ -46,29 +65,12 @@ export async function addFormData() {
             const reader = new FileReader()
             reader.addEventListener("load", function (e) {
                 const image = document.createElement("img")
-                image.src = e.target.result
+                image.src = reader.result
                 image.classList.add("uploadImage")
                 fileImage.innerHTML = ""
-                fileImage.appendChild(image)
+                fileImage.appendChild(image);
             })
             reader.readAsDataURL(file)
         }
     })
 }
-
-
-
-
-
-
-
-
-
-
-/*
-const dataList = document.getElementById("categorieList")
-function showList(e) {
-    if (e.keyCode === 40 && document.activeElement === input) {
-        dataList.style.display = 'block'
-    }
-}*/
