20336bed17f87c2a8a162a152f56129624a18a24
yoonju88 <yoonju.t@gmail.com>
2024-04-09 15:46:25 +0200

    resolve merge conflict

---
+++ FrontEnd/assets/button.js
@@ -0,0 +1,105 @@
+const baseUrl = "http://localhost:5678/api/"
+//Récup GET categories
+const getCategories = await fetch(baseUrl + "categories", { method: "GET" })
+const arrayCategories = await getCategories.json()
+
+//Récup GET works
+const getGallery = await fetch(baseUrl + "works", { method: "GET" })
+const galleries = await getGallery.json()
+
+export async function addgenerateButtons() {
+    
+    const btnFilters = document.querySelector(".btn-filters")
+    // correction error on login page
+    if (!btnFilters) { return }
+    const mapCategory = galleries.map(gallery => gallery.category)
+
+    function generateButtons() {
+
+        for (let i = 0; i < arrayCategories.length + 1; i++) {
+            const categoryLists = arrayCategories.find(category => category.id === i)
+            const button = document.createElement("button")
+            btnFilters.appendChild(button)
+
+            if (!categoryLists) {
+                button.textContent = "tous"
+                button.addEventListener("click", function () {
+                    const filterBtnAll = galleries.filter(function (gallery) {
+                        return mapCategory
+                    })
+                    document.querySelector(".gallery").innerHTML = ""
+                    generateGallery(filterBtnAll)
+                    console.log("display all", filterBtnAll)
+                })
+            } else {
+                button.textContent = categoryLists.name
+                button.addEventListener("click", function () {
+                    let categoryId = categoryLists.id
+                    const FilterButton = galleries.filter(function (gallery) {
+                        return gallery.category.id === categoryId
+                    })
+                    document.querySelector(".gallery").innerHTML = ""
+                    generateGallery(FilterButton)
+                    console.log("test filter", FilterButton)
+                })
+            }
+        }
+    }
+    generateButtons()
+
+    async function generateGallery(galleries) {
+        const sectionGallery = document.querySelector(".gallery")
+        // correction error on login page
+        if (!sectionGallery) { return }
+    
+        for (const gallery of galleries) {
+    
+            const figureElement = document.createElement("figure")
+            figureElement.dataset.categoryId = gallery.categoryId
+            figureElement.dataset.userId = gallery.userId
+            sectionGallery.appendChild(figureElement)
+    
+            const imageElement = document.createElement("img")
+            imageElement.src = gallery.imageUrl
+            imageElement.alt = gallery.title
+            figureElement.appendChild(imageElement)
+    
+            const figcaptionElement = document.createElement("figcaption")
+            figcaptionElement.innerText = gallery.title
+            figureElement.appendChild(figcaptionElement)
+        }
+    }
+    generateGallery(galleries);
+}
+/*
+export async function GenerateModalGallery() {
+
+    function modalGallery(galleries) {
+        const modalSectionGallery = document.querySelector(".modal-sectionGallery")
+        // correction error on login page
+        if (!modalSectionGallery) { return }
+
+        for (let i = 0; i < galleries.length; i++) {
+
+            const nGalleries = galleries[i]
+            let addIcone = `<a id="removeImage"><img src="./assets/icons/trash-can.png"></a>`
+            const figureElement = document.createElement("figure")
+            figureElement.dataset.categoryId = nGalleries.categoryId
+            figureElement.dataset.userId = nGalleries.userId
+            modalSectionGallery.appendChild(figureElement)
+            figureElement.innerHTML = addIcone
+
+            const imageElement = document.createElement("img")
+            imageElement.src = nGalleries.imageUrl
+            imageElement.alt = nGalleries.title
+            figureElement.appendChild(imageElement)
+        }
+    }
+    modalGallery(galleries);
+}
+*/
+
+
+
+
+
